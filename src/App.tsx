29e702f55d958da34aa9e203e43810a0565d83b4
rasberriali <julesalysa@gmail.com>
2025-06-04 15:06:10 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Main from "./mainpage/main"
+import Test from "./mainpage/test"
 
 function App() {
   return (
     <div className='inter '>
      <Main/>
+     {/* <Test/> */}
     </div>
   )
 }
--- src/mainpage/main.tsx
+++ src/mainpage/main.tsx
@@ -21,7 +21,7 @@ function Main() {
         <div className="flex-1 flex-row justify-between items-start ">
           <div className="flex flex-col justify-center xl:space-y-3 space-y-2">
             <div className="flex flex-row justify-between items-center">
-            <h1 className="xl:text-2xl  font-bold tracking-wide">
+            <h1 className="xl:text-2xl text-[14px]  font-bold tracking-wide">
               Alysa Emilio
             </h1>
            
@@ -38,18 +38,18 @@ function Main() {
             </h3>
 
             <div className="grid grid-cols-2 grid-rows-1 gap-3 xl:w-2/5 mt-2 ">
-            <div className="flex flex-row py-2 gap-2 px-2 bg-blue-400 text-center justify-center items-center rounded-[5px]">
+            <div className="flex flex-row xl:py- py-1 gap-2 px-2 bg-blue-400 text-center justify-center items-center rounded-[5px]">
                 <img src={email} alt="" className="w-4"></img>
 
-                <div className="xl:text-sm text-xs font-medium text-white">
+                <div className="xl:text-sm text-[10px] xl:font-medium text-white">
                     Send Email
                 </div>
             </div>
                 
-                 <div className="flex flex-row py-2 px-2 gap-2 ring ring-blue-100 text-center justify-center items-center rounded-[5px]">
+                 <div className="flex flex-row xl:py-2 py-1 px-2 gap-2 ring ring-blue-100 text-center justify-center items-center rounded-[5px]">
                 <img src={download} alt="" className="w-4"></img>
 
-                <div className="xl:text-sm text-xs font-medium">
+                <div className="xl:text-sm text-[10px] xl:font-medium">
                     Resume
                 </div>
             </div>
