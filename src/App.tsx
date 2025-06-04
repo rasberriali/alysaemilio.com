2da588315cf04c7334452a4b7785a3f67b49ef50
rasberriali <julesalysa@gmail.com>
2025-06-04 15:18:57 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Main from "./mainpage/main"
-import Test from "./mainpage/test"
 
 function App() {
   return (
     <div className='inter '>
      <Main/>
-     {/* <Test/> */}
     </div>
   )
 }
