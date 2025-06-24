8dd48d714f9b170036571e996bc47a3474f251f5
rasberriali <julesalysa@gmail.com>
2025-06-24 17:37:04 +0800

    resume fixes

--- src/mainpage/main.tsx
+++ src/mainpage/main.tsx
@@ -3,8 +3,8 @@ import profile from "../assets/files/profile.jpg"
 import email from "../assets/icons/email.svg"
 import Logos from "../mainpage/logos"
 import Bentogrids from "./bentogrids"
-import resume from "../assets/files/Resume.jpg"
-import resumepdf from "../assets/files/resumepdf.pdf"
+import resume from "../assets/files/Resume.png"
+import resumepdf from "../assets/files/alysaemilio-resume.pdf"
 
 
 interface MainProps {
